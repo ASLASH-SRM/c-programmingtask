5f5d2277700f41f5f19e95920775eff3928f679b
ASLASH-SRM <57229330+ASLASH-SRM@users.noreply.github.com>
2019-10-31 23:10:17 +0530

    Add files via upload

---
+++ vi personalinfo.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+
+{
+	char name[50];
+	int age;
+	int number;
+	printf("enter the name");
+	scanf("%s",&name);
+	printf("enter the mobile number\n ");
+	scanf("%d",&number);
+	printf("enter the age");
+	scanf("%d",&age);
+	printf("the name is %s\n",name);
+	printf("the age is %d\n",age);
+	printf("the mobile number is %d",number);
+}
+	
+	
+
+
